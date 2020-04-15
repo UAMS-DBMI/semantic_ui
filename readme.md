372ffa1b8b200b591d23e49d6f91e884af76cf2e
Joseph Utecht <thenmal@gmail.com>
2020-04-15 12:25:15 -0500

    added readme

---
+++ readme.md
@@ -0,0 +1,9 @@
+Semantic UI API
+===============
+
+To run
+------
+
+```pip install -r requirements.txt
+uvicorn semantic-api:app --reload```
+
--- semantic-api.py
+++ semantic-api.py
@@ -9,14 +9,6 @@ data = []
 with open('demo.json') as json_file:
     data = json.load(json_file)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
 @app.get("/demo")
 def demo():
     return data
