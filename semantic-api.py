c32244e4d5e6a81d83657c59588b89e2c2e5ba59
Joseph Utecht <thenmal@gmail.com>
2020-04-22 11:17:23 -0500

    moved to sparql data

--- semantic-api.py
+++ semantic-api.py
@@ -1,31 +1,125 @@
 #!/usr/bin/env python
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from collections import defaultdict
 import json
+import requests
+
+def make_sparql_query(query, params={}):
+    params = {'query': query}
+    headers = {'Accept': 'application/sparql-results+json'}
+    r = requests.get('http://localhost:7200/repositories/prism', params=params, headers=headers)
+    r.raise_for_status()
+    ret = []
+    vars = r.json()['head']['vars']
+    for row in r.json()['results']['bindings']:
+        new_row = {}
+        for var in vars:
+            new_row[var] = row[var]['value']
+        ret.append(new_row)
+    return ret
 
 app = FastAPI()
-data = []
-with open('demo.json') as json_file:
-    data = json.load(json_file)
 
-@app.get("/demo")
-def demo():
-    return data
+@app.get("/locations")
+def locations():
+    query = """PREFIX human: <http://purl.obolibrary.org/obo/NCBITaxon_9606>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX identifier: <http://purl.obolibrary.org/obo/IAO_0020000>
+PREFIX has_part: <http://purl.obolibrary.org/obo/BFO_0000051>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select distinct ?location{
+	# the person
+	?person rdf:type human: .
+    # parts of this person
+ 	?person has_part: ?ppart .
+    ?ppart rdf:type ?loctype .
+    ?loctype rdfs:label ?location .
+    # want only the immediate location type -- not superclasses like 'organ subunit'
+    FILTER NOT EXISTS{
+        ?x rdfs:subClassOf ?loctype.
+    }
+}"""
+    results = make_sparql_query(query)
+    return [x['location'] for x in results]
 
-@app.get("/stats")
-def stat_list():
-    resp = []
-    for item in data:
-        for key in item.keys():
-            if key not in resp:
-                resp.append(key)
-    return resp
+@app.get("/disease_types")
+def disease_types():
+    query = """PREFIX collection: <http://purl.org/PRISM_0000001>
+PREFIX inheres: <http://purl.obolibrary.org/obo/RO_0000052>
+PREFIX human: <http://purl.obolibrary.org/obo/NCBITaxon_9606>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX has_part: <http://purl.obolibrary.org/obo/BFO_0000051>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select distinct ?disease_type {
+	# the person
+	?person rdf:type human: .
+    ?dis_inst inheres: ?ppart .
+    ?dis_inst rdf:type ?dt .
+    ?dt rdfs:label ?disease_type .
+    # want only the immediate diease type
+    FILTER NOT EXISTS{
+        ?x rdfs:subClassOf ?dt.
+    }
+}"""
+    results = make_sparql_query(query)
+    return [x['disease_type'] for x in results]
 
-@app.get("/stats/{stat_name}")
-def stats(stat_name: str):
-    resp = defaultdict(int)
-    for item in data:
-        if stat_name in item.keys():
-            resp[item[stat_name]] += 1
-    return resp
+@app.get("/collections")
+def collections():
+    query = """PREFIX collection: <http://purl.org/PRISM_0000001>
+PREFIX human: <http://purl.obolibrary.org/obo/NCBITaxon_9606>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX has_part: <http://purl.obolibrary.org/obo/BFO_0000051>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select distinct ?collection {
+
+    # the collection
+    ?cid rdf:type collection: .
+    ?cid rdfs:label ?collection .
+}"""
+    results = make_sparql_query(query)
+    return [x['collection'] for x in results]
+
+@app.get("/")
+def all_data():
+    query = """PREFIX collection: <http://purl.org/PRISM_0000001>
+PREFIX inheres: <http://purl.obolibrary.org/obo/RO_0000052>
+PREFIX human: <http://purl.obolibrary.org/obo/NCBITaxon_9606>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX identifier: <http://purl.obolibrary.org/obo/IAO_0020000>
+PREFIX denotes: <http://purl.obolibrary.org/obo/IAO_0000219>
+PREFIX has_part: <http://purl.obolibrary.org/obo/BFO_0000051>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select ?collection ?patient_id ?disease_type ?location{
+
+    # the collection
+    ?cid rdf:type collection: .
+    ?cid rdfs:label ?collection .
+
+	# the person
+	?person rdf:type human: .
+	# the person identifier
+    ?id denotes: ?person .
+    ?id rdf:type identifier: .
+    ?id rdfs:label ?patient_id .
+
+    # parts of this person
+ 	?person has_part: ?ppart .
+    ?ppart rdf:type ?loctype .
+    ?loctype rdfs:label ?location .
+    # want only the immediate location type -- not superclasses like 'organ subunit'
+    FILTER NOT EXISTS{
+        ?x rdfs:subClassOf ?loctype.
+    }
+
+    ?dis_inst inheres: ?ppart .
+    ?dis_inst rdf:type ?dt .
+    ?dt rdfs:label ?disease_type .
+    # want only the immediate diease type
+    FILTER NOT EXISTS{
+        ?x rdfs:subClassOf ?dt.
+    }
+
+}"""
+    return make_sparql_query(query)
