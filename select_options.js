0fa4bf53b6e0f3ffddd955db056bd0b08267afea
Joseph Utecht <thenmal@gmail.com>
2021-03-15 10:52:56 -0500

    leftovers

---
+++ select_options.js
@@ -0,0 +1,154 @@
+export const SELECT_OPTIONS = [
+  {
+    "label": "NSCLC-Radiogenomics",
+    "type": "Collection"
+  },
+  {
+    "label": "NSCLC-Radiomics",
+    "type": "Collection"
+  },
+  {
+    "label": "CPTAC-LSCC",
+    "type": "Collection"
+  },
+  {
+    "label": "CPTAC-LUAD",
+    "type": "Collection"
+  },
+  {
+    "label": "NSCLC-Radiomics-Genomics",
+    "type": "Collection"
+  },
+  {
+    "label": "NSCLC-Radiomics-Interobserver1",
+    "type": "Collection"
+  },
+  {
+    "label": "Invasive Lung Mucinous Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Acinar Cell Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Solid Lung Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lung Non-Small Cell Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Mucinous Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lung Basaloid Squamous Cell  Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Papillary Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lung Enteric Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Squamous Cell Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Keratinizing Squamous Cell Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lung Non-Keratinizing Squamous Cell Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lung Large Cell Carcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lepidic Predominant Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "Lung Adenocarcinoma",
+    "type": "Disease"
+  },
+  {
+    "label": "middle lobe of right lung",
+    "type": "Location"
+  },
+  {
+    "label": "upper lobe of right lung",
+    "type": "Location"
+  },
+  {
+    "label": "lower lobe of left lung",
+    "type": "Location"
+  },
+  {
+    "label": "upper lobe of left lung",
+    "type": "Location"
+  },
+  {
+    "label": "lower lobe of right lung",
+    "type": "Location"
+  },
+  {
+    "label": "left lung hilus",
+    "type": "Location"
+  },
+  {
+    "label": "anatomical entity",
+    "type": "Location"
+  },
+  {
+    "label": "Stage IA3",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage IV",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage IIIB",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage I",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage IA",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage IIA",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage II",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage IIB",
+    "type": "Stage"
+  },
+  {
+    "label": "Stage IB",
+    "type": "Stage"
+  },
+  {
+    "label": "male",
+    "type": "Sex"
+  },
+  {
+    "label": "female",
+    "type": "Sex"
+  }
+];
